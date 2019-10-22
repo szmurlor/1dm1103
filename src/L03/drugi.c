e4c247a98469697f4a0c9ab3d0b1531f28e87bc0
Robert Szmurlo <robert.szmurlo@ee.pw.edu.pl>
2019-10-22 13:58:48 +0200

    Koniec seminarium ...

--- src/L03/drugi.c
+++ src/L03/drugi.c
@@ -4,9 +4,27 @@ double iloczyn(int n, double v[]) {
     int i;
     double p = 1;
 
-    for (i = 0; i < n; i++) {
+/*    for (i = 0; i < n; i++) {
         p = p * v[i];
     }
+*/
+    i = 0;
+    while(i < n) {
+        p = p * v[i];
+        i = i + 1;
+        /* i += 1; */
+        /* i++; */
+        /*++i; */
+    }
+    
+    /*
+    i = 0;
+    do {
+        p = p * v[i];
+        i = i + 1;
+        v = (i++)*3
+    } while(i < n);
+    */
 
     return p;
 }
@@ -33,19 +51,34 @@ void wypisz(int n, double v[]) {
     }
 }
 
-int main(int argc, char *argv[]) {
+/* Interpretacja danych użytkownika. */
+void wczytaj(int n, double v[], char *argv[]) {
     int i;
-    int n = argc-1;
-    double v[n];
-
-    /* Interpretacja danych użytkownika. */
     for (i = 0; i < n; i++) {
         v[i] = atof(argv[i+1]);
     }
+}
 
+void zapisz(int n, double v[]) {
+    int i;
+    FILE * fout;
+    fout = fopen("wektor.txt", "w");
+    for (i = 0; i < n; i++) {
+        fprintf(fout, "%lf\n", v[i]);
+    }
+    fclose(fout);
+}
+
+int main(int argc, char *argv[]) {
+    int n = argc-1;
+    double v[n];
+
+    wczytaj(n,v,argv);
     wypisz(n,v);
     printf("Suma = %lf\n", suma(n,v) );
     printf("Iloczyn = %lf\n", iloczyn(n,v) );
+
+    zapisz(n,v);
 
     return 0;
 }
---
+++ src/L03/szybki-test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int i = 3;
+    int v = (++i) * 2;
+    printf("%d\n", v);
+    return 0;
+}
